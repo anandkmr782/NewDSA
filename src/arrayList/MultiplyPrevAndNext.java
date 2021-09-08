8784e9632445b82bc67371d71367a970d2a5f4d5
Anand kumar <57858682+anandkmr782@users.noreply.github.com>
2021-09-08 01:42:04 -0700

    16th

--- src/arrayList/MultiplyPrevAndNext.java
+++ src/arrayList/MultiplyPrevAndNext.java
@@ -1,9 +1,11 @@
 package arrayList;
-
+import java.util.*;
 public class MultiplyPrevAndNext {
 
 	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
 		System.out.println("Anand");
+		
 
 	}
 
