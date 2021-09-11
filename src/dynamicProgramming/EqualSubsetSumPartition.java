8043b638393404bd0a554dbd2a577b64ec527fae
Anand kumar <57858682+anandkmr782@users.noreply.github.com>
2021-09-11 14:34:59 +0530

    23

--- src/dynamicProgramming/EqualSubsetSumPartition.java
+++ src/dynamicProgramming/EqualSubsetSumPartition.java
@@ -1,10 +1,49 @@
 package dynamicProgramming;
 
+import java.util.Scanner;
+
 public class EqualSubsetSumPartition {
 
 	public static void main(String[] args) {
-		
-
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int arr[]=new int[n];
+		for(int i=0;i<n;i++)
+		{
+			arr[i]=sc.nextInt();
+		}
+		System.out.println(equalSum(n,arr));
+		sc.close();
+	}
+	static boolean equalSum(int n,int[] arr)
+	{
+		int sum=0;
+		for(int i=0;i<n;i++)
+		{
+			sum=sum+arr[i];
+		}
+		if(sum%2!=0)
+		{
+			return false;
+		}else {
+			return subSetSum(arr,n,sum/2);
+		}
+	}
+	static boolean subSetSum(int[] arr,int n,int sum)
+	{
+		if(sum==0)
+		{
+			return true;
+		}
+		if(n==0 && sum!=0)
+		{
+			return false;
+		}
+		if(arr[n-1]>sum)
+		{
+			return subSetSum(arr,n-1,sum);
+		}
+		return subSetSum(arr,n-1,sum)||subSetSum(arr,n-1,sum-arr[n-1]);
 	}
 
 }
